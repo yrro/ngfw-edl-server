0c63ca76f90b6dda17a7cb92bac8539e16775c43
Sam Morris <sam@robots.org.uk>
2024-05-12 10:41:33 +0100

    Add gunicorn config file

---
+++ gunicorn.conf.py
@@ -0,0 +1,11 @@
+# This file is used to configure gunicorn when running from the container
+# image. As a result, a user can override any of these properties by providing
+# additional arguments to the container.
+
+bind = ["[::]:8080"]
+
+accesslog = "-"
+
+wsgi_app = "ngfw_edl_server:create_app()"
+
+# pylint: skip-file
