76c673156d3e84891f79f095e06568e39c710379
Sam Morris <sam@robots.org.uk>
2024-05-13 09:34:15 +0100

    Handle NXDOMAIN when resolving addresses

--- src/ngfw_edl_server/dns.py
+++ src/ngfw_edl_server/dns.py
@@ -2,6 +2,7 @@ from collections.abc import AsyncGenerator
 import logging
 
 from dns.asyncresolver import Resolver
+from dns.resolver import NXDOMAIN
 
 
 logger = logging.getLogger(__name__)
@@ -9,15 +10,21 @@ logger = logging.getLogger(__name__)
 
 async def query(qname: str) -> AsyncGenerator[tuple[str, str], None]:
     resolver = Resolver()
+
     srv_answer = await resolver.resolve(
         qname, "srv", raise_on_no_answer=True, lifetime=5, search=False
     )
     logger.debug("Response: %r", srv_answer.response.to_text())
+
     for srv_rec in srv_answer:
         logger.debug("Answer RR: %r", srv_rec)
-        host_answers = await resolver.resolve_name(
-            srv_rec.target, raise_on_no_answer=True, lifetime=5, search=False
-        )
-        for address in host_answers.addresses():
-            logger.debug("Address: %r", address)
-            yield address, srv_rec.target.to_text()
+        try:
+            host_answers = await resolver.resolve_name(
+                srv_rec.target, raise_on_no_answer=True, lifetime=5, search=False
+            )
+        except NXDOMAIN:
+            logger.warning("%r has no addresses")
+        else:
+            for address in host_answers.addresses():
+                logger.debug("Address: %r", address)
+                yield address, srv_rec.target.to_text()
