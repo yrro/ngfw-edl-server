0af43dacc314aa944e26df372f675b5aa8001081
Sam Morris <sam@robots.org.uk>
2024-05-14 12:17:31 +0100

    document behaviour

--- src/ngfw_edl_server/server.py
+++ src/ngfw_edl_server/server.py
@@ -10,6 +10,13 @@ blueprint = Blueprint("server", __name__)
 @blueprint.route("/srv/<target>")
 async def srv(target: str) -> ResponseReturnValue:
 
+    # IP Address Lists formatting guidelines:
+    # <https://docs.paloaltonetworks.com/pan-os/9-1/pan-os-admin/policy/use-an-external-dynamic-list-in-policy/formatting-guidelines-for-an-external-dynamic-list/ip-address-list#idd44a975a-a94a-4398-864e-5cf223f1d351>
+    #
+    # If the target specified is not found in the DNS, we want to return a 500
+    # error so that the firewall does not empty out a previously working IP
+    # address list. Hence we do not catch the NXDOMAIN error that would be
+    # raised by dns.query.
     content = ""
     async for addr, comment in dns.query(target):
         content += addr + "  " + comment
